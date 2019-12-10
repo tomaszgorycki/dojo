fe935788f433dc5fdd607300c4236f83a7f3083e
Franciszek Jaworski <franciszekjaworski@gmail.com>
2019-12-10 10:13:11 +0100

    merge

--- gittie.py
+++ gittie.py
@@ -3,14 +3,14 @@
 def introduce():
     print("Hello, I'm Gittie!")
 
-<<<<<<< HEAD
+
+def joke():
+    print("knock, knock, who's there? - me, Gittie!")
+
 def add(a,b):
     c=a+b
     return(c)
 
 c=add(3,5)
 print(c)
-=======
-def joke():
-    print("knock, knock, who's there? - me, Gittie!")
->>>>>>> 60c722dbee81d39b2ed93beec8767655d13a14a6
+
